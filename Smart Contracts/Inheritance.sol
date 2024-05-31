889171dba9a552feee8af6a66972ce45e1491b95
Saksham Tewari <sakshamtewari3@gmail.com>
2024-05-31 06:56:55 +0530

    Deploying one smart contract(SecretVault) from the other(MyContract2)

--- Smart Contracts/Inheritance.sol
+++ Smart Contracts/Inheritance.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+
 contract Ownable {
 
     address owner;
@@ -27,6 +28,38 @@ contract MyContract is Ownable {
 
     function getSecret() public view onlyOwner returns (string memory){
         return secret;
+    }
+
+}
+
+
+//Now creating another smart contract 'SecretVault' which will be deployed through 'MyContract2'
+//We will use getSecret() function from that contract
+
+contract SecretVault {
+
+    string secret;
+    constructor(string memory _secret) public {
+        secret = _secret;
+    }
+    function getSecret() public view returns (string memory){
+        return secret;
+    }
+}
+
+
+contract MyContract2 is Ownable {
+
+    address secretVault;        //address of SecretVault contract as it will be used to use its functions
+
+    constructor(string memory _secret) public {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);       //storing address of the 'new' SecretVault contract
+        super;  //call the constructor of parent contract (Ownable);
+    }
+
+    function getSecret() public view onlyOwner returns (string memory){
+        return SecretVault(secretVault).getSecret();
     }
 
 }

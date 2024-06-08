ccf6af3d2cf2d2b26aea887ee9d94c9ce277ca2a
Saksham Tewari <sakshamtewari3@gmail.com>
2024-06-08 23:50:15 +0530

    Added modifier 'restricted' , created 'Request' struct

--- CrowdFund/CrowdFund.sol
+++ CrowdFund/CrowdFund.sol
@@ -1,9 +1,21 @@
 pragma solidity ^0.8.0;
 
 contract CrowdFund {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    Request[] public requests;
     address public manager;    
     uint public minimumContribution;
     address[] public approvers;  //people who will approve manager's spending request
+
+    function restricted(){
+        require(msg.sender == manager);
+        _;
+    }
 
     // person can call this function to start the campaign
     function Campaign(uint minimum) public {
@@ -15,5 +27,10 @@ contract CrowdFund {
     function contribute() public {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender)   // if person has sent good amount, add him/her to approvers list
+    }
+
+    //allow manager to create a request
+    function createRequest() public restricted(); {
+        
     }
 }

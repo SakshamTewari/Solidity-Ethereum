1b5e6aa5875fe7f1020b12079545c05f27d50249
Saksham Tewari <sakshamtewari3@gmail.com>
2024-06-09 10:22:55 +0530

    Added function 'createRequest' , fixed errors

--- CrowdFund/CrowdFund.sol
+++ CrowdFund/CrowdFund.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
 contract CrowdFund {
@@ -12,7 +13,7 @@ contract CrowdFund {
     uint public minimumContribution;
     address[] public approvers;  //people who will approve manager's spending request
 
-    function restricted(){
+    modifier restricted(){
         require(msg.sender == manager);
         _;
     }
@@ -20,17 +21,24 @@ contract CrowdFund {
     // person can call this function to start the campaign
     function Campaign(uint minimum) public {
         manager = msg.sender;          //person who starts the campaign will be the manager
-        minimumContribution = minimum  //Minimum contribtuion required for anyone who wants to contribute
+        minimumContribution = minimum;  //Minimum contribtuion required for anyone who wants to contribute
     }
 
     //people who want to contribute can call this function and send amount
-    function contribute() public {
+    function contribute() payable public {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender)   // if person has sent good amount, add him/her to approvers list
+        approvers.push(msg.sender);   // if person has sent good amount, add him/her to approvers list
     }
 
     //allow manager to create a request
-    function createRequest() public restricted(); {
-        
+    function createRequest(string memory _description, uint _value, address _recipient) public restricted {
+        Request memory newRequest = Request({
+            description: _description,
+            value: _value,
+            recipient: _recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
     }
 }

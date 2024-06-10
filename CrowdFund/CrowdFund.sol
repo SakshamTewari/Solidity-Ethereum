28766d203c44e6392b064dee6e3f1c8495c79d58
Saksham Tewari <sakshamtewari3@gmail.com>
2024-06-10 20:14:00 +0530

    Updated commands

--- CrowdFund/CrowdFund.sol
+++ CrowdFund/CrowdFund.sol
@@ -7,11 +7,14 @@ contract CrowdFund {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;  //to keep a track of yes votes the request has received : not keeping 'no' count
+        mapping(address => bool) approvals;  //to keep addresses of poople who has voted;
     }
     Request[] public requests;
     address public manager;    
     uint public minimumContribution;
-    address[] public approvers;  //people who will approve manager's spending request
+    // address[] public approvers;  //people who will approve manager's spending request
+    mapping(address => bool) public approvers;  //to store if person has voted
 
     modifier restricted(){
         require(msg.sender == manager);
@@ -27,7 +30,8 @@ contract CrowdFund {
     //people who want to contribute can call this function and send amount
     function contribute() payable public {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);   // if person has sent good amount, add him/her to approvers list
+        // approvers.push(msg.sender);   // if person has sent good amount, add him/her to approvers list
+        approvers[msg.sender] = true;
     }
 
     //allow manager to create a request
@@ -36,7 +40,10 @@ contract CrowdFund {
             description: _description,
             value: _value,
             recipient: _recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
+            //  we only need to initialise 'value types' and not 'reference type' , hence 
+            // no mapping initialisation
         });
 
         requests.push(newRequest);

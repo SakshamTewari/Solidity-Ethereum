bdca0369fdaad9cbfdc7fb79f4887a2e5786ce86
Saksham Tewari <sakshamtewari3@gmail.com>
2024-06-11 17:27:38 +0530

    Add getVendingMachineBalance function

--- Smart Contracts/VendingMachine.sol
+++ Smart Contracts/VendingMachine.sol
@@ -7,8 +7,12 @@ contract VendingMachine {
     address public owner;
     mapping(address => uint) public donutBalances;
 
-    constructor {
+    constructor() {
         owner = msg.sender;
         donutBalances[address(this)] = 100;   // set the balance of 100 to the contract address
+    }
+
+    function getVendingMachingBalance() public view returns(uint){
+        return donutBalances[address(this)];
     }
 }

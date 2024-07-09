509c507755765783afcf727a43321aaca6ef1f64
Saksham Tewari <sakshamtewari3@gmail.com>
2024-07-09 23:36:16 +0530

    Chain Validation using isValid()

--- Alchemy/Blockchain Cryptography/blockchain_network.js
+++ Alchemy/Blockchain Cryptography/blockchain_network.js
@@ -195,3 +195,55 @@ class Blockchain {
 }
 
 module.exports = Blockchain;
+
+/*
+
+Chain Validation
+----------------
+
+Blockchains are run by a network of computers. When a computer finds a new block, it broadcasts its new version of the blockchain to all of its peers. There may be multiple versions of the blockchain at any given time. 
+However, the longest valid blockchain is the accepted one.
+
+
+Create a function called isValid on our Blockchain that will return true or false if a block is valid or invalid respectively
+isValid should check the integrity of every block in its chain by looking at each block's previousHash field and making sure that it is equal to the hash of the block before it
+
+To compare the output of the SHA256 function you will need to convert it into a string (.toString) before comparing. Example:
+
+const hash1 = SHA256("a");
+const hash2 = SHA256("a");
+
+console.log(hash1 === hash2); // false
+console.log(hash1.toString() === hash2.toString()); // true
+
+Notice that first one is false! 
+These two are objects and are compared by reference which is why we need to convert it to a string!
+*/
+
+const Block = require('./Block');
+
+class Blockchain {
+  constructor() {
+    this.chain = [new Block()];
+    [
+      /* TODO: Create the genesis block here */
+    ];
+  }
+
+  addBlock(block) {
+    block.previousHash = this.chain[this.chain.length - 1].toHash();
+    return this.chain.push(block);
+  }
+
+  isValid() {
+    for (let i = this.chain.length - 1; i > 0; i--) {
+      let block = this.chain[i],
+        prev = this.chain[i - 1];
+      if (block.previousHash.toString() !== prev.toHash().toString())
+        return false;
+    }
+    return true;
+  }
+}
+
+module.exports = Blockchain;

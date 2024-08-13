27e201c67a4f63779042a8cc5a6bc9ad9492f386
Saksham Tewari <sakshamtewari3@gmail.com>
2024-08-13 22:22:10 +0530

    Added : function 'pay'

--- BlockChain_Ecommerce_App/contracts/PaymentProcessor.sol
+++ BlockChain_Ecommerce_App/contracts/PaymentProcessor.sol
@@ -11,9 +11,21 @@ contract PaymentProcessor {
     // using 'DAI' coin as it is always stable [1 DAI = 1 USD]
     IERC20 public dai;
 
+    // even when payment is done
+    event PaymentDone(address payer, uint amount, uint paymentId, uint date);
+
     // address of dai will be different here as we are deploying it on public testnet
     constructor(address adminAddress, address daiAddress) {
         admin = adminAddress;
         dai = IERC20(daiAddress);
+    }
+
+    // function 'pay'
+    // amount = amount of dai token that is transferred
+    // transfers token from 'buyer' to 'admin' [merchant]
+    // tokens will never be in our smart contract
+    function pay(uint amount, uint paymentId) external {
+        dai.transferFrom(msg.sender, admin, amount);
+        emit PaymentDone(msg.sender, amount, paymentId, block.timestamp);
     }
 }

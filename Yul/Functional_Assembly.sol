f108f0a33237fcbef7053b32438c34a48f912e32
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-24 19:51:05 +0530

    Native loops

--- Yul/Functional_Assembly.sol
+++ Yul/Functional_Assembly.sol
@@ -1,9 +1,19 @@
-function asmLoops() public returns (uint_r) {
-    assembly {
-        let i := 0
-    loop:
-        i := add(i, 1)
-        _r := add(_r, 1)
-        jumpi(loop, lt(i, 10))
+pragma solidity ^0.8.0;
+
+contract Assembly {
+    function nativeLoops() public returns (uint _r) {
+        for (uint i = 0; i < 10; i++) {
+            _r++;
+        }
+    }
+
+    function asmLoops() public returns (uint_r) {
+        assembly {
+            let i := 0
+        loop:
+            i := add(i, 1)
+            _r := add(_r, 1)
+            jumpi(loop, lt(i, 10))
+        }
     }
 }
